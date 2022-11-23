0a0c654dec1ae3c9f4c0fc33bf84cf94d9f41a15
wersoam <cowistrash@gmail.com>
2022-11-23 09:55:36 -0500

    ahhhhhhhh

--- main.js
+++ main.js
@@ -2,24 +2,24 @@ const DOMSelectors = {
   button: document.getElementById("btn"),
   text: document.querySelector("#text"),
   input: document.getElementById("input"),
-  text2: document.getElementById("#text2"),
   img: document.getElementById("image"),
   button2: document.getElementById("btn2"),
-  button3: document.getElementById("btn3"),
 };
 function text(text) {
   text.textContent = "step 1 done";
 }
+function clearinput() {
+  DOMSelectors.input.value = "";
+}
+function display() {
+  DOMSelectors.img.insertAdjacentHTML("beforeend", (img.src = "pizza.jpg"));
+}
 DOMSelectors.button.addEventListener("click", function () {
   text(DOMSelectors.text);
   var answer = document.getElementById("input").value;
-  DOMSelectors.input.value = "";
-  console.log(answer);
+if var answer = "pizza" {display()}
 });
 
-function hotdog() {
-  img.src = "hotdog.jpg";
-}
 function pizza() {
   img.src = "pizza.jpg";
 }
